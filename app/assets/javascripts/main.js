678e9b9612ecc2c9769a7e0c581d46a53f9a378b
lethe <lethe2211@gmail.com>
2014-05-11 03:53:50 +0900

    set handler on hover and change node into circle

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,10 +7,13 @@
 (function($){
 
   var Renderer = function(canvas){
-    var canvas = $(canvas).get(0)
+      var canvas = $(canvas).get(0);
 
     var ctx = canvas.getContext("2d");
-    var particleSystem
+      var particleSystem;
+
+      var flag = false;
+      var hovered = null;
 
     var that = {
       init:function(system){
@@ -25,15 +28,15 @@
         // inform the system of the screen dimensions so it can map coords for us.
         // if the canvas is ever resized, screenSize should be called again with
         // the new dimensions
-        particleSystem.screenSize(canvas.width, canvas.height) 
+        particleSystem.screenSize(canvas.width, canvas.height)
         particleSystem.screenPadding(80) // leave an extra 80px of whitespace per side
-        
+
         // set up some event handlers to allow for node-dragging
         that.initMouseHandling()
       },
-      
-      redraw:function(){
-        // 
+
+      redraw:function(e){
+        //
         // redraw will be called repeatedly during the run whenever the node positions
         // change. the new positions for the nodes can be accessed by looking at the
         // .p attribute of a given node. however the p.x & p.y values are in the coordinates
@@ -41,10 +44,21 @@
         // the screen yourself, or use the convenience iterators .eachNode (and .eachEdge)
         // which allow you to step through the actual node objects but also pass an
         // x,y point in the screen's coordinate system
-        // 
+        //
         ctx.fillStyle = "white"
         ctx.fillRect(0,0, canvas.width, canvas.height)
-        
+
+
+          if(flag == true) {
+	      //console.log('posx: ' + hovered.point.x);
+	      //console.log('posy: ' + hovered.point.y);
+	      //console.log('distance: ' + hovered.distance);
+	      //console.log('name: ' + hovered.node.name);
+	      ctx.fillStyle = "black";
+	      ctx.fillText(hovered.node.name, hovered.point.x, hovered.point.y);
+
+	  }
+
         particleSystem.eachEdge(function(edge, pt1, pt2){
           // edge: {source:Node, target:Node, length:#, data:{}}
           // pt1:  {x:#, y:#}  source position in screen coords
@@ -66,16 +80,22 @@
           // draw a rectangle centered at pt
           var w = 10
           ctx.fillStyle = (node.data.alone) ? "orange" : "black"
-          ctx.fillRect(pt.x-w/2, pt.y-w/2, w,w)
-        })    			
+          //ctx.fillRect(pt.x-w/2, pt.y-w/2, w,w)
+          ctx.beginPath();
+          ctx.arc(pt.x, pt.y, 10, 0, 2 * Math.PI, false);
+          ctx.stroke();
+          ctx.fill();
+          if(flag == 1 && hovered.node.name == node.name){
+            ctx.fillStyle = "black";
+        ctx.fillText(hovered.node.name, pt.x+10, pt.y-10);
+          }
+        })
       },
-      
+
       initMouseHandling:function(){
         // no-nonsense drag and drop (thanks springy.js)
         var dragged = null;
 
-
-	  var hovered = null;
 	  var handler = {
 
 	      hovered:function(e) {
@@ -83,13 +103,15 @@
 		  var pos = $(canvas).offset();
 		  var _mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top);
 		  hovered = particleSystem.nearest(_mouseP);
-		  
-		  console.log('x: ' + hovered.point.x);
-		  console.log('y: ' + hovered.point.y);
-		  console.log('distance: ' + hovered.distance);
-		  if(hovered.distance < 50) {
-		      ctx.fillRect(hovered.point.x, hovered.point.y, 1, 1);
+
+      flag = (hovered.distance < 20)
+/*		  if(hovered.distance < 50) {
+		      flag = true;
 		  }
+		  else {
+		      flag = false;
+		  }
+*/
 		  return false;
 	      }
 
@@ -100,54 +122,54 @@
         // set up a handler object that will initially listen for mousedowns then
         // for moves and mouseups while dragging
 
-        // var handler = {
-        //   clicked:function(e){
-        //     var pos = $(canvas).offset();
-        //     _mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
-        //     dragged = particleSystem.nearest(_mouseP);
+        handler = {
+          clicked:function(e){
+            var pos = $(canvas).offset();
+            _mouseP = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
+            dragged = particleSystem.nearest(_mouseP);
 
-        //     if (dragged && dragged.node !== null){
-        //       // while we're dragging, don't let physics move the node
-        //       dragged.node.fixed = true
-        //     }
+            if (dragged && dragged.node !== null){
+              // while we're dragging, don't let physics move the node
+              dragged.node.fixed = true
+            }
 
-        //     $(canvas).bind('mousemove', handler.dragged)
-        //     $(window).bind('mouseup', handler.dropped)
+            $(canvas).bind('mousemove', handler.dragged)
+            $(window).bind('mouseup', handler.dropped)
 
-        //     return false
-        //   },
-        //   dragged:function(e){
-        //     var pos = $(canvas).offset();
-        //     var s = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
+            return false
+          },
+          dragged:function(e){
+            var pos = $(canvas).offset();
+            var s = arbor.Point(e.pageX-pos.left, e.pageY-pos.top)
 
-        //     if (dragged && dragged.node !== null){
-        //       var p = particleSystem.fromScreen(s)
-        //       dragged.node.p = p
-        //     }
+            if (dragged && dragged.node !== null){
+              var p = particleSystem.fromScreen(s)
+              dragged.node.p = p
+            }
 
-        //     return false
-        //   },
+            return false
+          },
 
-        //   dropped:function(e){
-        //     if (dragged===null || dragged.node===undefined) return
-        //     if (dragged.node !== null) dragged.node.fixed = false
-        //     dragged.node.tempMass = 1000
-        //     dragged = null
-        //     $(canvas).unbind('mousemove', handler.dragged)
-        //     $(window).unbind('mouseup', handler.dropped)
-        //     _mouseP = null
-        //     return false
-        //   }
-        // }
-        
-        // // start listening
-        // $(canvas).mousedown(handler.clicked);
+          dropped:function(e){
+            if (dragged===null || dragged.node===undefined) return
+            if (dragged.node !== null) dragged.node.fixed = false
+            dragged.node.tempMass = 1000
+            dragged = null
+            $(canvas).unbind('mousemove', handler.dragged)
+            $(window).unbind('mouseup', handler.dropped)
+            _mouseP = null
+            return false
+          }
+        }
+
+        // start listening
+        $(canvas).mousedown(handler.clicked);
 
       },
-      
+
     }
     return that
-  }    
+  }
 
   $(document).ready(function(){
     var sys = arbor.ParticleSystem(1000, 600, 0.5) // create the system with sensible repulsion/stiffness/friction
@@ -166,7 +188,7 @@
     // sys.graft({
     //   nodes:{
     //     f:{alone:true, mass:.25}
-    //   }, 
+    //   },
     //   edges:{
     //     a:{ b:{},
     //         c:{},
@@ -175,7 +197,7 @@
     //     }
     //   }
     // })
-    
+
   })
 
 })(this.jQuery)
