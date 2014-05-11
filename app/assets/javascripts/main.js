188d9e317f7b0dfe3b6fea742735a6915b7373da
lethe <lethe2211@gmail.com>
2014-05-11 16:28:35 +0900

    challenge changing edges into arcs

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -48,6 +48,7 @@
         ctx.fillStyle = "white"
         ctx.fillRect(0,0, canvas.width, canvas.height)
 
+	  var nodeBoxes = {}
 
           if(flag == true) {
 	      //console.log('posx: ' + hovered.point.x);
@@ -64,6 +65,15 @@
           // pt1:  {x:#, y:#}  source position in screen coords
           // pt2:  {x:#, y:#}  target position in screen coords
 
+	    var weight = edge.data.weight
+          var color = edge.data.color
+
+            if (!color || (""+color).match(/^[ \t]*$/)) color = null
+
+          // find the start point
+            // var tail = intersect_line_box(pt1, pt2, nodeBoxes[edge.source.name])
+            // var head = intersect_line_box(tail, pt2, nodeBoxes[edge.target.name])
+
           // draw a line from pt1 to pt2
           ctx.strokeStyle = "rgba(0,0,0, .333)"
           ctx.lineWidth = 1
@@ -71,23 +81,59 @@
           ctx.moveTo(pt1.x, pt1.y)
           ctx.lineTo(pt2.x, pt2.y)
           ctx.stroke()
+
+	    // // draw an arrowhead if this is a -> style edge
+            // if (edge.data.directed){
+	    // 	ctx.save()
+            //   // move to the head position of the edge we just drew
+	    // 	var wt = !isNaN(weight) ? parseFloat(weight) : 1
+	    // 	var arrowLength = 6 + wt
+	    // 	var arrowWidth = 2 + wt
+	    // 	ctx.fillStyle = (color) ? color : "#cccccc"
+	    // 	ctx.translate(head.x, head.y);
+	    // 	ctx.rotate(Math.atan2(head.y - tail.y, head.x - tail.x));
+
+            //   // delete some of the edge that's already there (so the point isn't hidden)
+	    // 	ctx.clearRect(-arrowLength/2,-wt/2, arrowLength/2,wt)
+
+            //   // draw the chevron
+	    // 	ctx.beginPath();
+	    // 	ctx.moveTo(-arrowLength, arrowWidth);
+	    // 	ctx.lineTo(0, 0);
+	    // 	ctx.lineTo(-arrowLength, -arrowWidth);
+	    // 	ctx.lineTo(-arrowLength * 0.8, -0);
+	    // 	ctx.closePath();
+	    // 	ctx.fill();
+	    // 	ctx.restore()
+            // }
+
         })
 
         particleSystem.eachNode(function(node, pt){
           // node: {mass:#, p:{x,y}, name:"", data:{}}
           // pt:   {x:#, y:#}  node position in screen coords
 
+	    if (node.data.shape=='dot'){
+		gfx.oval(pt.x-w/2, pt.y-w/2, w,w, {fill:ctx.fillStyle})
+		nodeBoxes[node.name] = [pt.x-w/2, pt.y-w/2, w,w]
+            }else{
+		gfx.rect(pt.x-w/2, pt.y-10, w,20, 4, {fill:ctx.fillStyle})
+		nodeBoxes[node.name] = [pt.x-w/2, pt.y-11, w, 22]
+            }
+
+	    console.log(node.name);
+
           // draw a rectangle centered at pt
           var w = 10
           ctx.fillStyle = (node.data.alone) ? "orange" : "black"
           //ctx.fillRect(pt.x-w/2, pt.y-w/2, w,w)
           ctx.beginPath();
-          ctx.arc(pt.x, pt.y, 10, 0, 2 * Math.PI, false);
+          ctx.arc(pt.x, pt.y, 3, 0, 2 * Math.PI, false);
           ctx.stroke();
           ctx.fill();
           if(flag == 1 && hovered.node.name == node.name){
-            ctx.fillStyle = "black";
-        ctx.fillText(hovered.node.name, pt.x+10, pt.y-10);
+              ctx.fillStyle = "black";
+              ctx.fillText(hovered.node.name, pt.x+10, pt.y-10);
           }
         })
       },
@@ -176,24 +222,25 @@
     sys.parameters({gravity:true}) // use center-gravity to make the graph settle nicely (ymmv)
     sys.renderer = Renderer("#citation_graph") // our newly created renderer will have its .init() method called shortly by sys...
 
-      var data = {
-	  nodes:{
-	      animals:{'color':'red','shape':'dot','label':'Animals'},
-	      dog:{'color':'green','shape':'dot','label':'dog'},
-	      cat:{'color':'blue','shape':'dot','label':'cat'}
-	  },
-	  edges:{
-	      animals:{ dog:{}, cat:{} }
-	  }
-      };
-      sys.graft(data);
+      // var data = {
+      // 	  nodes:{
+      // 	      animals:{'color':'red','shape':'dot','label':'Animals'},
+      // 	      dog:{'color':'green','shape':'dot','label':'dog'},
+      // 	      cat:{'color':'blue','shape':'dot','label':'cat'}
+      // 	  },
+      // 	  edges:{
+      // 	      animals:{ dog:{}, cat:{} }
+      // 	  }
+      // };
+      // sys.graft(data);
+      // sys.addEdge('cat', 'dog', {directed: true, weight: 3});
 
     // add some nodes to the graph and watch it go...
-    // sys.addEdge('a','b')
-    // sys.addEdge('a','c')
-    // sys.addEdge('a','d')
-    // sys.addEdge('a','e')
-    // sys.addNode('f', {alone:true, mass:.25})
+    sys.addEdge('a','b')
+    sys.addEdge('a','c')
+    sys.addEdge('a','d')
+      sys.addEdge('a','e', {directed: true, weight: 3})
+    sys.addNode('f', {alone:true, mass:.25})
 
     // or, equivalently:
     //
@@ -211,5 +258,35 @@
     // })
 
   })
+
+    // helpers for figuring out where to draw arrows (thanks springy.js)
+    var intersect_line_line = function(p1, p2, p3, p4)
+    {
+	var denom = ((p4.y - p3.y)*(p2.x - p1.x) - (p4.x - p3.x)*(p2.y - p1.y));
+	if (denom === 0) return false // lines are parallel
+	var ua = ((p4.x - p3.x)*(p1.y - p3.y) - (p4.y - p3.y)*(p1.x - p3.x)) / denom;
+	var ub = ((p2.x - p1.x)*(p1.y - p3.y) - (p2.y - p1.y)*(p1.x - p3.x)) / denom;
+
+	if (ua < 0 || ua > 1 || ub < 0 || ub > 1)  return false
+	return arbor.Point(p1.x + ua * (p2.x - p1.x), p1.y + ua * (p2.y - p1.y));
+    }
+
+    var intersect_line_box = function(p1, p2, boxTuple)
+    {
+	var p3 = {x:boxTuple[0], y:boxTuple[1]},
+        w = boxTuple[2],
+          h = boxTuple[3]
+
+	var tl = {x: p3.x, y: p3.y};
+	var tr = {x: p3.x + w, y: p3.y};
+	var bl = {x: p3.x, y: p3.y + h};
+	var br = {x: p3.x + w, y: p3.y + h};
+
+	return intersect_line_line(p1, p2, tl, tr) ||
+            intersect_line_line(p1, p2, tr, br) ||
+            intersect_line_line(p1, p2, br, bl) ||
+            intersect_line_line(p1, p2, bl, tl) ||
+            false
+    }
 
 })(this.jQuery)
