3117ac8c009f570122b1c5257e6cebdaf7cd8e92
lethe <lethe2211@gmail.com>
2014-06-10 20:28:00 +0900

    introduce cache to google_scholar_citedby

--- lib/crawler/google_scholar_citedby.py
+++ lib/crawler/google_scholar_citedby.py
@@ -6,6 +6,8 @@ import requests
 import commands
 
 from google_scholar_bibliography import *
+import filecache
+
 from bs4 import BeautifulSoup
 
 def get_citedby(cluster_id):
@@ -17,39 +19,56 @@ def get_citedby(cluster_id):
     ネットワーク関係の何か？(API制限とか)
     printを挟んで処理を遅延させるとうまくいく(？)
     '''
-    art = get_bibliography(cluster_id) # 書誌情報を返す
 
-    citedbyes = [] # 被引用論文のcluster_id
-    if art["url_citations"][0] is not None:
-        html = requests.get(art["url_citations"][0]).text
-        #print html
-        soup = BeautifulSoup(html)
-        soup_titles = soup.find_all("h3", "gs_rt")
-        for soup_title in soup_titles:
-            # 各被引用論文について，タイトルで検索した時の上位1件のcluster_idを取得
-            citedby_title = soup_title.a.get_text() if soup_title.a else ''
-            #print citedby_title
-            # citedby_cmd = os.path.dirname(sys.argv[0]) + "/scholarpy/scholar.py -c 1 -t --csv --phrase " # CSV形式で検索
-            # citedby_cmd += '"' + citedby_title + '"' 
-            # csv = commands.getoutput(citedby_cmd)
-            # #print csv
-            # citedby_cid = csv.split('|')[5] if len(csv.split('|')) >= 6 else None  # 6番目がcluster_id
+    abspath = os.path.dirname(os.path.abspath(__file__))
+    citedby = filecache.Client(abspath + '/citedby/')
 
-            querier = ScholarQuerierWithSnippets()
-            settings = ScholarSettings()
-            querier.apply_settings(settings)
-            query = SearchScholarQuery()
-            query.set_words(citedby_title.encode('utf-8'))
-            query.set_scope(True)
-            query.set_num_page_results(1) # 返す検索結果は1件
-            querier.send_query(query)
-            result = put_json(querier)
-            citedby_cid = result["cluster_id"][0]
+    cache = citedby.get(str(cluster_id))
+    if cache != None and cache['status'] == 'OK':
+        return cache['data']
+    else:
+        art = get_bibliography(cluster_id) # 書誌情報を返す
 
-            if citedby_cid is not None:
-                citedbyes.append(citedby_cid)
+        result = {'status': '', 'data': []} # 被引用論文のcluster_id
 
-    return citedbyes
+        if art["url_citations"][0] is not None:
+            html = requests.get(art["url_citations"][0]).text
+            #print html
+            soup = BeautifulSoup(html)
+            soup_titles = soup.find_all("h3", "gs_rt")
+            for soup_title in soup_titles:
+                # 各被引用論文について，タイトルで検索した時の上位1件のcluster_idを取得
+                citedby_title = soup_title.a.get_text() if soup_title.a else ''
+                # print citedby_title
+                # citedby_cmd = os.path.dirname(sys.argv[0]) + "/scholarpy/scholar.py -c 1 -t --csv --phrase " # CSV形式で検索
+                # citedby_cmd += '"' + citedby_title + '"' 
+                # csv = commands.getoutput(citedby_cmd)
+                # #print csv
+                # citedby_cid = csv.split('|')[5] if len(csv.split('|')) >= 6 else None  # 6番目がcluster_id
+
+                querier = ScholarQuerierWithSnippets()
+                settings = ScholarSettings()
+                querier.apply_settings(settings)
+                query = SearchScholarQuery()
+                query.set_words(citedby_title.encode('utf-8'))
+                query.set_scope(True)
+                query.set_num_page_results(1) # 返す検索結果は1件
+                querier.send_query(query)
+                res = put_json(querier)
+                citedby_cid = res["cluster_id"][0]
+
+                if citedby_cid is not None:
+                    result['data'].append(citedby_cid)
+
+        # とりあえず結果が空でないならOKとする(←あまりよくない…)
+        if len(result['data']) != 0:
+            result['status'] = 'OK'
+            citedby.set(str(cluster_id), result)
+        else:
+            result['status'] = 'NG'
+
+
+        return result['data']
 
 if __name__ == '__main__':
     if len(sys.argv) == 2 and sys.argv[1] != '':
