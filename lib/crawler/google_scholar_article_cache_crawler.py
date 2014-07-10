0bde68d1a41de1510b4f00e0ee800f2f9517c656
lethe <lethe2211@gmail.com>
2014-07-11 03:11:50 +0900

    add google scholar article cache crawler

---
+++ lib/crawler/google_scholar_article_cache_crawler.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+import logging
+
+from google_scholar_article_crawler import GoogleScholarArticleCrawler
+
+class GoogleScholarArticleCacheCrawler(object):
+
+    def __init__(self):
+        pass
+
+    def crawl(self, cluster_id, depth=10):
+        print cluster_id
+        g = GoogleScholarArticleCrawler()
+        g.get_bibliography(cluster_id)
+        citation = g.get_citation(cluster_id)
+        citedby = g.get_citedby(cluster_id)
+
+        if depth == 0:
+            return
+        else:
+            for c in citation:
+                self.crawl(c, depth - 1)
+
+            if citation == []:
+                for c in citedby:
+                    self.crawl(c, depth - 1)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    if len(sys.argv) == 2:
+        g = GoogleScholarArticleCacheCrawler()
+        print g.crawl(sys.argv[1], depth=1)
