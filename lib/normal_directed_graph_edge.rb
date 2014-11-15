7d10396994a608dd5e467c227025636bed96589d
lethe <lethe2211@gmail.com>
2014-11-15 16:41:45 +0900

    modify CitationGraphComposer#compute_graph (WIP)

--- lib/normal_directed_graph_edge.rb
+++ lib/normal_directed_graph_edge.rb
@@ -4,7 +4,7 @@
 # 非検索結果論文ノード同士や，非検索結果論文ノードと検索結果論文ノードをつなぐエッジ
 class NormalDirectedGraphEdge < DirectedGraphEdge
   def initialize(source, destination, weight, bibliography)
-    super(source, destionation, weight, '#cccccc', bibliography)
+    super(source, destination, weight, '#cccccc', bibliography)
   end
 end
 
--- lib/search_result_graph.rb
+++ lib/search_result_graph.rb
@@ -14,60 +14,84 @@ class SearchResultGraph < CacheableGraph
   def append_node(node)
     # NormalGraphNodeにSearchResultGraphNodeを上書きすることは可能だが，逆はできないようにする
     case node
-      when SearchResultGraphNode
+    when SearchResultGraphNode
       unless exists_search_result_node?(node.id)
         @graph[:nodes][node.id.to_s] = node
       end
-      when NormalGraphNode
+    when NormalGraphNode
       unless exists_node?(node.id)
         @graph[:nodes][node.id.to_s] = node
       end
     end
   end
 
+  def exists_normal_edge?(source, destination)
+    return (exists_edge?(source, destination) and NormalDirectedGraphEdge === @graph[:edges][source.to_s][destination.to_s])
+  end
+
+  def exists_between_search_results_edge(source, destination)
+    return (exists_edge?(source, destination) and BetweenSearchResultsDirectedGraphEdge === @graph[:edges][source.to_s][destination.to_s])
+  end
+
   def append_edge(edge)
-    
+    # NormalDirectedGraphEdgeにBetweenSearchResultsDirectedGraphEdgeを上書きすることは可能だが，逆はできないようにする
+    if exists_node?(edge.source.to_s) and exists_node?(edge.destination.to_s)     
+      case edge
+      when BetweenSearchResultsDirectedGraphEdge
+        unless @graph[:edges].key?(edge.source.to_s)
+          @graph[:edges][edge.source.to_s] = {}
+        end
+        @graph[:edges][edge.source.to_s][edge.destination.to_s] = edge
+      when NormalDirectedGraphEdge
+        unless exists_edge?(edge.source, edge.destination)
+          unless @graph[:edges].key?(edge.source.to_s)
+            @graph[:edges][edge.source.to_s] = {}
+          end
+          @graph[:edges][edge.source.to_s][edge.destination.to_s] = edge
+        end
+      end
+    end
   end
 end
 
-if __FILE__ == $PROGRAM_NAME
-  require_relative './json_cache.rb'
+  if __FILE__ == $PROGRAM_NAME
+    require_relative './json_cache.rb'
 
-  require 'active_support/core_ext'
-  bib1 = {"status" => "OK", "data" => {"title" => "title1", "year" => "year1"}}
-  bib2 = {"status" => "OK", "data" => {"title" => "title2", "year" => "year2"}}
-  bib3 = {"status" => "OK", "data" => {"title" => "title3", "year" => "year3"}}
-  n1 = GraphNode.new(1, 10, bib1)
-  n2 = SearchResultGraphNode.new(2, 20, bib2, 5)
-  n3 = SearchResultGraphNode.new(3, 30, bib3, 1)
-  n4 = SearchResultGraphNode.new(1, 30, bib3, 1)
-  e1 = DirectedGraphEdge.new(1, 2, 5, "#cccccc", {})
-  e2 = UndirectedGraphEdge.new(2, 3, 10, "#dddddd", {})
-  e3 = UndirectedGraphEdge.new(1, 2, 10, "#dddddd", {})
+    require 'active_support/core_ext'
+    bib1 = {"status" => "OK", "data" => {"title" => "title1", "year" => "year1"}}
+    bib2 = {"status" => "OK", "data" => {"title" => "title2", "year" => "year2"}}
+    bib3 = {"status" => "OK", "data" => {"title" => "title3", "year" => "year3"}}
+    n1 = GraphNode.new(1, 10, bib1)
+    n2 = SearchResultGraphNode.new(2, 20, bib2, 5)
+    n3 = SearchResultGraphNode.new(3, 30, bib3, 1)
+    n4 = SearchResultGraphNode.new(1, 30, bib3, 1)
+    e1 = DirectedGraphEdge.new(1, 2, 5, "#cccccc", {})
+    e2 = UndirectedGraphEdge.new(2, 3, 10, "#dddddd", {})
+    e3 = UndirectedGraphEdge.new(1, 2, 10, "#dddddd", {})
 
-  gc = SearchResultGraph.new('1')
-  gc.append_node(n1)
-  p gc.to_h
-  gc.append_node(n2)
-  p gc.to_h
-  gc.append_node(n3)
-  p gc.to_h
-  gc.append_node(n4)
-  p gc.to_h
-  gc.delete_node(3)
-  p gc.to_h
-  gc.append_edge(e1)
-  p gc.to_h
-  gc.append_edge(e2)
-  p gc.to_h
-  gc.append_edge(e3)
-  p gc.to_h
-  gc.delete_edge(1, 2)
-  p gc.to_h
-  gc.set_cache
-  gc2 = SearchResultGraph.new('1')
-  puts gc2
-  p gc2.to_h
-  gc3 = SearchResultGraph.new('2')
-  p gc3.to_h
-end
+    gc = SearchResultGraph.new('1')
+    gc.append_node(n1)
+    p gc.to_h
+    gc.append_node(n2)
+    p gc.to_h
+    gc.append_node(n3)
+    p gc.to_h
+    gc.append_node(n4)
+    p gc.to_h
+    gc.delete_node(3)
+    p gc.to_h
+    gc.append_edge(e1)
+    p gc.to_h
+    gc.append_edge(e2)
+    p gc.to_h
+    gc.append_edge(e3)
+    p gc.to_h
+    gc.delete_edge(1, 2)
+    p gc.to_h
+    gc.set_cache
+    gc2 = SearchResultGraph.new('1')
+    puts gc2
+    p gc2.to_h
+    gc3 = SearchResultGraph.new('2')
+    p gc3.to_h
+  end
